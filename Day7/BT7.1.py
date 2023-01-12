2174add476adebce63def137bc2fcb15056d7b3e
= <=>
2023-01-12 23:02:54 +0700

    update

--- Day7/BT7.1.py
+++ Day7/BT7.1.py
@@ -10,19 +10,19 @@ print(art_students & math_students) #toán tử
 #Tìm những người bạn học vẽ nhưng không học toán
 set2 = art_students.difference(math_students)
 print(set2)
-print(art_students-math_students) #Toán tử
+print(art_students - math_students) #Toán tử
 
 #Tìm những người bạn học toán nhưng không học vẽ
 set3 = math_students.difference(art_students)
 print(set3)
-print(math_students-art_students) #Toán tử
+print(math_students - art_students) #Toán tử
 
 #Tìm những người bạn học vẽ hay toán không phải cả hai
 set4 = art_students.symmetric_difference(math_students)
 print(set4)
-print(art_students^math_students) #toán tử
+print(art_students ^ math_students) #toán tử
 
 #Tìm tất cả những người bạn
 set5 = art_students.union(math_students)
 print(set5)
-print(art_students|math_students)
+print(art_students | math_students)
--- Day7/BT7.2.py
+++ Day7/BT7.2.py
@@ -30,16 +30,19 @@ print(release_year_value)
 #Thay đổi giá trị của key: release_year từ 1973 thành 1971
 album_info["release_year"] = 1971
 print(album_info)
+
 #Xóa phần tử với key là track_list
 del album_info["track_list"]
 print(album_info)
+
 # Thêm một key mới là amount = 2000 bằng hai cách
-album_info.update(amount="2000")
+album_info.update(amount=2000)
 print(album_info)
 
-info = {'amount': '2000'}
+info = {'amount': 2000}
 album_info.update(info)
 print(album_info)
+
 # Làm trống dict: album_info
 album_info.clear()
 print(album_info)
