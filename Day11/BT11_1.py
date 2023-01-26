c6b8826be8d3b863d88259cfeb31772148632d26
= <=>
2023-01-26 17:12:14 +0700

    Update

---
+++ Day11/BT11_1.py
@@ -0,0 +1,36 @@
+# Định nghĩa 4 hàm add, subtract, divide, multiply (+, -, /, *).
+# Mỗi hàm nhận vào hai tham số thực hiện lần lượt các phép toán cộng, trừ, chia, nhân. Lưu ý hàm nên trả về thay vì in ra.
+
+# add
+def my_add(x, y):
+    return (x + y)
+
+
+print(my_add(6, 3))
+
+# subtract
+
+
+def my_subtract(a, b):
+    return (a-b)
+
+
+print(my_subtract(6, 3))
+
+# divide
+
+
+def my_subtract(c, d):
+    return (c/d)
+
+
+print(my_subtract(6, 3))
+
+# multiply
+
+
+def my_subtract(e, f):
+    return (e*f)
+
+
+print(my_subtract(6, 3))
---
+++ Day11/BT11_2.py
@@ -0,0 +1,15 @@
+# Định nghĩa một hàm được gọi là print_show_info nhận vào một tham số duy nhất đó là một dict, lúc gọi thì sử dụng dict như sau:
+
+tv_show = {
+    "title": "Breaking Bad",
+    "seasons": 5,
+    "initial_release": 2008
+}
+
+
+def print_show_info(tv_show):
+    print(
+        f"{tv_show['title']} ({tv_show['initial_release']}) - {tv_show['seasons']} seasons")
+
+
+print(print_show_info(tv_show))
